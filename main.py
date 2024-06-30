c005774b9975146e8b11b3e957e664dbffb40ace
Alexander <aik63@tpu.ru>
2024-06-30 17:08:35 +0700

    added queue control

--- main.py
+++ main.py
@@ -1,21 +1,70 @@
 import streamlit as st
 import datetime
+import time
+import threading
+
+from sympy import per
 import model_inference
 from PIL import Image
+from queue_handling import RequestQueue
 
 
-# Multi select
-selections = st.multiselect("Photo style:", ["Kuindzhi", "Kandinsky", "Malevich"])
+# Глобальный экземпляр RequetsQueue
+REQUEST_QUEUE = None
 
-# Number input
-choice = st.number_input("Choose the number of epochs", 0, 50)
 
-# File upload
-data = st.file_uploader("Share png file", type=['png'])
+def get_request_queue():
+    global REQUEST_QUEUE
+    
+    if REQUEST_QUEUE is None:
+        REQUEST_QUEUE = RequestQueue()
 
-if data != None:
-     edited_image = model_inference.inference_edit_image(data)
+    return REQUEST_QUEUE
 
-     # User image show
-     st.image(edited_image, caption="Your image", use_column_width=True)
 
+def periodic_result_fetch(request_queue, request_id):
+     textholder = st.empty()
+     textholder.write(f"Текущий размер очереди: {request_queue.queue.qsize()}")
+     while True:
+          time.sleep(1)
+          done_request = request_queue.fetch_result(request_id)
+          if done_request != None:
+               st.write("Done!")
+               return done_request
+          
+          textholder.write(f"Текущий размер очереди: {request_queue.queue.qsize()}")
+
+
+def main():
+     # Получение или создание экземпляра очереди запросов
+     request_queue = get_request_queue()
+     
+     # Multi select
+     selections = st.multiselect("Photo style:", ["Kuindzhi", "Kandinsky", "Malevich"])
+
+     # Number input
+     choice = st.number_input("Choose the number of epochs", 0, 50)
+
+     # File upload
+     data = st.file_uploader("Share png file", type=['png'])
+
+     if data != None:
+          #edited_image = model_inference.inference_edit_image(data)
+
+          # User image show
+          st.image(data, caption="Your image", use_column_width=True)
+
+
+          if st.button("Process image"):
+               # Добавление в очередь запросов
+               req_id, eta_sec = request_queue.add_to_queue(selections, data)
+               
+               st.write(f"Добавлен запрос с id: {req_id}")
+               st.write(f"Ожидаемое время ожидания: {eta_sec} секунд")
+               
+               result = periodic_result_fetch(request_queue, req_id)
+               st.image(result["result"], caption="Edited image", use_column_width=True)
+
+
+if __name__ == "__main__":
+     main()
---
+++ queue_handling.py
@@ -0,0 +1,65 @@
+import time
+import threading
+import uuid
+from queue import Queue
+
+import model_inference
+
+
+class RequestQueue:
+    def __init__(self):
+        # Создаем очередь для обработки запросов
+        self.queue = Queue()
+        
+        # Список для хранения результатов
+        self.requests_results = []
+        
+        # Запуск потока обработки запросов в очереди
+        self.process_requests_thread = threading.Thread(target=self._process_requests, daemon=True)
+        self.process_requests_thread.start()
+    
+      
+    # Функция для обработки запросов в очереди
+    def _process_requests(self):
+        while True:
+            time.sleep(1)
+            if not self.queue.empty():
+                request = self.queue.get()
+                result = self.edit_image(request["image"])
+                request["result"] = result
+                self.requests_results.append(request)
+                
+                
+    # Функция для запуска обработки изображения
+    def edit_image(self, image):
+        edited_image = model_inference.inference_edit_image(image)
+        time.sleep(5)
+        
+        return edited_image
+    
+    
+    # Пытаемся извлечь результат запроса из списка результатов
+    def fetch_result(self, request_id):
+        for request in self.requests_results:
+            if request["request_id"] == request_id:
+                return request
+    
+        return None
+    
+    
+    # Добавление в очередь запросов
+    def add_to_queue(self, request_params, image):
+        # Создаем id запроса
+        request_id = str(uuid.uuid4())
+        # Добавляем запрос в очередь
+        request = {"request_id": request_id, "time": time.time(), 
+                   "params": request_params, "image": image}
+        self.queue.put(request)
+
+        # Отображаем информацию о состоянии очереди
+        eta_sec = 5
+        queue_size = self.queue.qsize()
+        if queue_size > 0:
+            eta_sec = queue_size * 5  # Предполагаемое время ожидания (в секундах)
+
+        return request_id, eta_sec
